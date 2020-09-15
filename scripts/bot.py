d9436e2d20c3d7c3b56bd78d7e4e65bc83f03fa2
Julio Bonon <juliocesarbonon@gmail.com>
2020-09-14 22:28:36 -0300

    Add server login route

--- scripts/bot.py
+++ scripts/bot.py
@@ -5,7 +5,7 @@ import json
 import time
 import warnings
 
-class bot():
+class Bot():
     warnings.filterwarnings('ignore')
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
@@ -17,22 +17,38 @@ class bot():
     GRID_CONTAINER_START = 1
     GRID_CONTAINER_FINAL = 8
 
-    def site_login(self): 
-        self.driver.get("https://siga.cps.sp.gov.br/aluno/login.aspx?") #pega a url do siga e passa pro driver
+    def site_login(self, rg, password): 
+        self.driver.get("https://siga.cps.sp.gov.br/aluno/login.aspx") #pega a url do siga e passa pro driver
         time.sleep(2)
+        self.driver.find_element_by_id("vSIS_USUARIOID").send_keys(rg) #pega as informações sobre o login
+        self.driver.find_element_by_id("vSIS_USUARIOSENHA").send_keys(password) #input de senha
+        self.driver.find_element_by_name("BTCONFIRMA").click()
         try:
-            self.driver.find_element_by_id("vSIS_USUARIOID").send_keys(self.final_data['username']) #pega as informações sobre o login
-            self.driver.find_element_by_id("vSIS_USUARIOSENHA").send_keys(self.final_data['password']) #input de senha
-            self.driver.find_element_by_name("BTCONFIRMA").click()
+            self.find_page()
+            return 0
         except:
-            print("Login failed")
+            return 1
 
     def find_page(self):
         time.sleep(2)
         self.driver.get("https://siga.cps.sp.gov.br/aluno/home.aspx") #acha o link de redirecionamento pra página das notas e clica
         self.driver.find_element_by_id("ygtvlabelel10").click()
         time.sleep(2)
-
+    
+    def get_profile_data(self):
+        div = self.driver.find_element_by_id("TABLE1_MPAGE")
+        html = div.get_attribute("innerHTML")
+        soup = BeautifulSoup(html, 'html.parser')
+        imgtable = soup.find("td", {"style": "text-align:-khtml-center"})
+        img = imgtable.find("img")
+        spantable = soup.find("table", {"class": "Tb_CorpoCaixas"})
+        span_nome = spantable.find("span", {"id": "span_MPW0041vPRO_PESSOALNOME"})
+        dict = {
+            'img': img['src'], 
+            'nome': span_nome.text
+        }
+        print(dict)
+        
     def parse_grades(self):
         time.sleep(2)
         self.driver.get("https://siga.cps.sp.gov.br/aluno/notasparciais.aspx")
@@ -46,20 +62,38 @@ class bot():
                 cont = soup.find("table", {"id": "Grid2Container_000" +str(i) + "Tbl"})
                 print(grade.text)
                 for item in cont.find_all("tr", {"class": "tableborderOdd"}):
-                    dict = {}
+                    dict= {}
                     gradename = item.find("td", {"valign": "top"})
                     grade_number = item.find("td", {"valign": "middle"})
                     dict['id'] = str(i)
-                    gradenameformatted = gradename.text
                     try:
-                        grade_number_formatted = grade_number.text
+                        dict['gradenumber'] = grade_number.text
                     except:
-                        grade_number_formatted = "Sem Nota"
+                        dict['gradenumber'] = "Sem Nota"
                     dict['grade'] = grade.text
-                    dict['gradename'] = gradenameformatted
-                    dict['gradenumber'] = grade_number_formatted
+                    dict['gradename'] = gradename.text
                     list.append(dict)
                 print(i)
                 print('\n')
         self.driver.close()
         return list
+    
+    def organize_grades(self, list):
+        grade_list = []
+        for item in list:
+            grade_list.append(item['grade'])
+        
+        i = 1
+        new_list = []
+        for item in list:
+            i =+ i
+            if item['grade'] == grade_list[i]:
+                dict = {}
+                dict[grade_list[1]]= {
+                    'notanome': item['gradename'],
+                    'gradenumber': item['gradenumber']
+                }
+                new_list.append(dict)
+        
+        print(new_list)
+    
--- scripts/server.py
+++ scripts/server.py
@@ -1,26 +1,36 @@
-from bot import bot
+from bot import Bot
 from flask import Flask, request, jsonify
 from os import sys
 
-if __name__ == "__main__":
-    ip = sys.argv[1]
-    bot = bot()
-    bot.site_login()
-    bot.find_page()
-    list = []
-    list = bot.parse_grades()
-    print(list)
-    app = Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>Siga</h1>
 <p>Uma API para retornar dados do SIGA</p>'''
 
-
 @app.route('/grades', methods=['GET'])
 def api_gatry():
     return jsonify(list), 200
 
+@app.route('/login', methods=['POST'])
+def login():
+    login = request.form.get('rg')
+    password = request.form.get('password')
+    bot = Bot()
 
-app.run(host=ip)
+    msg = ''
+    if (bot.site_login(login, password) == 0):
+        msg = 'Success'
+        return jsonify(msg), 200
+    else:
+        msg = 'Failure'
+        return jsonify(msg), 404
+
+    
+       
+    
+if __name__ == "__main__":
+    ip = sys.argv[1]
+    app.run(host=ip)
+
