ac52ded0c59dcc68c201e088411d9992121e9e69
Julio Bonon <juliocesarbonon@gmail.com>
2021-02-18 10:26:32 -0300

    Add animation to the loadingscreen.dart

--- lib/components/loadingscreen.dart
+++ lib/components/loadingscreen.dart
@@ -1,31 +1,58 @@
 import 'package:flutter/material.dart';
 
-class LoadingScreen extends StatelessWidget {
+class LoadingContainer extends StatefulWidget {
+  @override
+  _LoadingContainerState createState() => _LoadingContainerState();
+}
+
+class _LoadingContainerState extends State<LoadingContainer>
+    with TickerProviderStateMixin {
+  AnimationController controller;
+
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+      lowerBound: 0,
+      upperBound: 100,
+    );
+
+    controller.addListener(() {
+      setState(() {});
+    });
+
+    controller.repeat(reverse: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 200,
+        margin: EdgeInsets.only(top: controller.value),
+        child: Image.asset(
+          'imgs/siga.png',
+        ));
+  }
+}
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            AnimatedContainer(
-              width: 200,
-              child: Image.asset(
-                'imgs/siga.png',
-              ),
-              duration: Duration(seconds: 1),
-              curve: Curves.bounceIn,
-            ),
-            CircularProgressIndicator(
-              strokeWidth: 2,
-              backgroundColor: Colors.red,
-            ),
-            SizedBox(height: 20),
-            Text('Carregando, aguarde...'),
-          ],
-        ),
+        body: Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          LoadingContainer(),
+        ],
       ),
-    );
+    ));
   }
 }
