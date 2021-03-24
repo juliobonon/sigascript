cf8e93a7ccd8cd14cfcbac2cd74e042e7be0bdc2
Julio Cesar Bonon <juliocesarbonon@gmail.com>
2021-03-23 22:23:54 -0300

    Add formType and state to the login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,24 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:sigascript/register.dart';
+import 'package:sigascript/components/formButton.dart';
+import 'package:sigascript/components/inputContainer.dart';
 import 'package:sigascript/services/auth.dart';
+import 'package:sigascript/services/emailValidator.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
-  LoginPage({this.auth, this.onSignedIn});
+  LoginPage({this.auth, this.onSignedIn, this.validator});
   final BaseAuth auth;
   final VoidCallback onSignedIn;
+  final Validator validator;
+
   @override
   _LoginPageState createState() => _LoginPageState();
 }
+
+enum FormType { login, register }
 
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   final String universityName = 'Fatec Campinas';
   TextEditingController _email = new TextEditingController();
   TextEditingController _password = new TextEditingController();
+  TextEditingController _user = TextEditingController();
+  TextEditingController _repeatPassword = TextEditingController();
 
-  bool validateAndSave() {
+  FormType _formType = FormType.login;
+
+  bool validateForm() {
     final form = _formKey.currentState;
     if (form.validate()) {
       form.save();
@@ -29,27 +39,95 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   void validateAndSubmit() async {
-    if (validateAndSave()) {
-      try {
-        User userId = await widget.auth
-            .signInWithEmailAndPassword(_email.text, _password.text);
-        print('Signed in: ' + userId.uid);
-        widget.onSignedIn();
-      } catch (e) {
-        print(e);
+    if (validateForm()) {
+      if (_formType == FormType.login) {
+        try {
+          User userId = await widget.auth
+              .signInWithEmailAndPassword(_email.text, _password.text);
+          print("Signed in: " + userId.uid);
+          widget.onSignedIn();
+        } catch (e) {
+          print(e);
+        }
+      } else {
+        try {
+          User userId = await widget.auth
+              .createUserWithEmailandPassword(_email.text, _password.text);
+          print('Signed in: ' + userId.uid);
+          widget.onSignedIn();
+        } catch (e) {
+          print(e);
+        }
       }
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    if (_formType == FormType.login) {
+      return loginForm(widget.validator);
+    } else {
+      return registerForm(widget.validator);
+    }
+  }
+
+  Widget loginForm(Validator validator) {
     return Scaffold(
       body: Center(
         child: SingleChildScrollView(
           child: Form(
             key: _formKey,
             child: Column(
-              children: buildLoginForm(),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'imgs/fatec.png',
+                    width: 300,
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                InputContainer(
+                  name: "Email",
+                  controller: _email,
+                  validator: validator.validateEmail,
+                  obscureText: false,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                InputContainer(
+                  name: "Senha",
+                  controller: _password,
+                  validator: validator.validatePassword,
+                  obscureText: true,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                FormButton(
+                  name: "Login",
+                  function: validateAndSubmit,
+                ),
+                SizedBox(height: 10),
+                FormButton(
+                  name: "Registrar",
+                  function: () {
+                    setState(() {
+                      _formType = FormType.register;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                InkWell(
+                  child: Text('Made with ❤️ by juliobonon'),
+                  onTap: () => launch('https://github.com/juliobonon'),
+                ),
+              ],
             ),
           ),
         ),
@@ -57,141 +135,78 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  List<Widget> buildLoginForm() {
-    return [
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Image.asset(
-          'imgs/fatec.png',
-          width: 300,
-        ),
-      ),
-      SizedBox(
-        height: 50,
-      ),
-      Container(
-        width: 330,
-        height: 60,
-        padding: EdgeInsets.only(
-          left: 10,
-        ),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10), color: Colors.grey[200]),
-        child: TextFormField(
-          style: TextStyle(color: Colors.red),
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.all(4),
-            focusColor: Colors.red,
-            labelText: 'Digite seu email',
-            labelStyle: TextStyle(color: Colors.black),
-            border: InputBorder.none,
-          ),
-          controller: _email,
-          validator: (value) {
-            if (value.isEmpty) {
-              return 'Informe o e-mail';
-            }
-          },
-        ),
-      ),
-      SizedBox(
-        height: 10,
-      ),
-      Container(
-        width: 330,
-        height: 60,
-        padding: EdgeInsets.only(left: 10),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10), color: Colors.grey[200]),
-        child: TextFormField(
-          style: TextStyle(
-            color: Colors.red,
-          ),
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.all(4),
-            labelText: 'Senha',
-            border: InputBorder.none,
-            labelStyle: TextStyle(color: Colors.black),
-          ),
-          controller: _password,
-          obscureText: true,
-          validator: (value) {
-            if (value.isEmpty) {
-              return 'Digite sua senha';
-            }
-          },
-        ),
-      ),
-      SizedBox(
-        height: 20,
-      ),
-      ButtonTheme(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        minWidth: 250,
-        height: 50,
-        child: ElevatedButton.icon(
-          icon: Icon(Icons.arrow_forward_ios_rounded),
-          style: ButtonStyle(
-            padding: MaterialStateProperty.all(
-              EdgeInsets.only(
-                top: 15,
-                left: 59,
-                right: 59,
-                bottom: 15,
-              ),
-            ),
-            backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
-          ),
-          label: Text(
-            "Login",
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-          onPressed: () {
-            validateAndSubmit();
-          },
-        ),
-      ),
-      SizedBox(height: 10),
-      ButtonTheme(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        minWidth: 200,
-        height: 50,
-        child: ElevatedButton.icon(
-            icon: Icon(Icons.arrow_forward_ios_rounded),
-            style: ButtonStyle(
-              padding: MaterialStateProperty.all(
-                EdgeInsets.only(
-                  top: 15,
-                  left: 40,
-                  right: 45,
-                  bottom: 15,
-                ),
-              ),
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
-            ),
-            label: Text(
-              "Registrar",
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Register(
-                    auth: widget.auth,
-                    onSignedIn: widget.onSignedIn,
+  Widget registerForm(Validator validator) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 40, right: 40),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: IconButton(
+                  icon: Icon(
+                    Icons.arrow_back_rounded,
+                    size: 40,
                   ),
+                  onPressed: () {
+                    setState(
+                      () {
+                        _formType = FormType.login;
+                      },
+                    );
+                  },
                 ),
-              );
-            }),
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Crie uma conta",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(height: 20),
+              InputContainer(
+                name: "User",
+                controller: _user,
+                validator: validator.validatePassword,
+                obscureText: false,
+              ),
+              SizedBox(height: 10),
+              InputContainer(
+                name: "Email",
+                controller: _email,
+                validator: validator.validateEmail,
+                obscureText: false,
+              ),
+              SizedBox(height: 10),
+              InputContainer(
+                name: "Senha",
+                controller: _password,
+                validator: validator.validatePassword,
+                obscureText: true,
+              ),
+              SizedBox(height: 10),
+              InputContainer(
+                name: "Digite sua senha novamente",
+                controller: _repeatPassword,
+                validator: validator.validatePassword,
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+              FormButton(
+                name: "Registrar",
+                function: validateAndSubmit,
+              ),
+            ],
+          ),
+        ),
       ),
-      SizedBox(
-        height: 20,
-      ),
-      InkWell(
-        child: Text('Made with ❤️ by juliobonon'),
-        onTap: () => launch('https://github.com/juliobonon'),
-      ),
-    ];
+    );
   }
 }
