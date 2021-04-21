287b527dc85160d5475849d57e4692b0eb66653b
Julio Cesar Bonon <juliocesarbonon@gmail.com>
2021-04-20 23:33:57 -0300

    rootPage.dart: Add AuthWrapper class

--- lib/rootPage.dart
+++ lib/rootPage.dart
@@ -1,60 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sigascript/home.dart';
 import 'package:sigascript/login.dart';
-import 'package:sigascript/routemanager.dart';
 import 'package:sigascript/services/validator.dart';
-import 'services/auth.dart';
 
-class RootPage extends StatefulWidget {
-  RootPage({this.auth, this.validator});
-  final BaseAuth auth;
-  final Validator validator;
-  @override
-  _RootPageState createState() => _RootPageState();
-}
-
-enum AuthStatus { notSignedIn, signedIn }
-
-class _RootPageState extends State<RootPage> {
-  AuthStatus authStatus = AuthStatus.notSignedIn;
-
-  @override
-  void initState() {
-    super.initState();
-    widget.auth.currentUser().then((userId) {
-      setState(() {
-        authStatus =
-            userId == null ? AuthStatus.notSignedIn : AuthStatus.signedIn;
-      });
-    });
-  }
-
-  void _signedIn() {
-    setState(() {
-      authStatus = AuthStatus.signedIn;
-    });
-  }
-
-  void _signedOut() {
-    setState(() {
-      authStatus = AuthStatus.notSignedIn;
-    });
-  }
-
+class AuthWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    switch (authStatus) {
-      case AuthStatus.notSignedIn:
-        return new LoginPage(
-          auth: widget.auth,
-          onSignedIn: _signedIn,
-          validator: widget.validator,
-        );
-      case AuthStatus.signedIn:
-        return new HomePage(
-          auth: widget.auth,
-          onSignedOut: _signedOut,
-        );
+    final firebaseUser = context.watch<User>();
+
+    if (firebaseUser != null) {
+      return HomePage();
+    } else {
+      return LoginPage(
+        validator: new Validator(),
+      );
     }
   }
 }
