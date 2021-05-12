b53ee46d46c6d5a3c0f0c5959e52acfd87f17405
Julio Cesar Bonon <juliocesarbonon@gmail.com>
2021-05-12 17:46:36 -0300

    Add alertDialog.dart

---
+++ lib/components/alertDialog.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+showAlertDialog(BuildContext context, String text) {
+  // set up the button
+  Widget okButton = TextButton(
+    child: Text("OK"),
+    onPressed: () {
+      Navigator.pop(context);
+    },
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Alerta"),
+    content: Text(text),
+    actions: [
+      okButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
