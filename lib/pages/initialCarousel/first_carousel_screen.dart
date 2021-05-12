57ec91fd96430bbf46abf5ace322b4a24b528db8
Julio Cesar Bonon <juliocesarbonon@gmail.com>
2021-05-12 17:46:51 -0300

    Add carousel screens

---
+++ lib/pages/initialCarousel/first_carousel_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          SizedBox(height: 40),
+          Image.asset(
+            'imgs/075.png',
+            width: 240,
+            height: 250,
+          ),
+          SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Text(
+              'Acesse suas notas \n diretamente do celular!',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18.0),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
