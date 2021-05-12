55320eb1e882838131513c93193a685089d2db27
Julio Cesar Bonon <juliocesarbonon@gmail.com>
2021-05-12 17:47:25 -0300

    Add carousel

--- lib/pages/initialCarousel/carousel.dart
+++ lib/pages/initialCarousel/carousel.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:sigascript/pages/initialCarousel/learnAboutSiga.dart';
+import 'package:sigascript/pages/initialCarousel/first_carousel_screen.dart';
+import 'package:sigascript/pages/initialCarousel/second_carousel_screen.dart';
 
 class Carousel extends StatelessWidget {
   @override
@@ -9,13 +10,41 @@ class Carousel extends StatelessWidget {
       body: Center(
         child: Container(
           width: 400,
-          height: 400,
-          child: CarouselSlider(
-            items: [LearnAboutSiga()],
-            options: CarouselOptions(
-              height: 400,
+          height: 500,
+          child: Column(children: [
+            CarouselSlider(
+              items: [
+                FirstScreen(),
+                SecondScreen(),
+              ],
+              options: CarouselOptions(
+                height: 400,
+              ),
             ),
-          ),
+            SizedBox(height: 20),
+            ButtonTheme(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              minWidth: 250,
+              height: 50,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 10),
+                  child: Text(
+                    'Comece a usar',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/loginPage');
+                },
+              ),
+            ),
+          ]),
         ),
       ),
     );
