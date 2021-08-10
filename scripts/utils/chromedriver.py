9386e08287d135cbc093f640fdd304015ae06ec5
Julio Bonon <juliocesarbonon@gmail.com>
2021-08-09 21:38:58 -0300

    Update utils/chromedriver.py

--- scripts/utils/chromedriver.py
+++ scripts/utils/chromedriver.py
@@ -5,11 +5,15 @@ import requests
 
 headers = {
     'authority': 'chromedriver.storage.googleapis.com',
-    'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
+    'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91",'
+    '"Chromium";v="91"',
     'sec-ch-ua-mobile': '?0',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
     'accept': '*/*',
-    'x-client-data': 'CIe2yQEIo7bJAQipncoBCI+5ygEIjYjLAQjxl8sBCKCgywEI3fLLAQjq8ssBCO/yywEIzfbLAQiz+MsBCKf5ywEI8PnLAQiv+ssBCO/6ywEYjp7LARi68ssBGI/1ywE=',
+    'x-client-data': 'CIe2yQEIo7bJAQipncoBCI+5ygEIjYjLAQjxl8sBCKCgywEI3'
+    'fLLAQjq8ssBCO/yywEIzfbLAQiz+MsBCKf5y'
+    'wEI8PnLAQiv+ssBCO/6ywEYjp7LARi68ssBGI/1ywE=',
     'sec-fetch-site': 'same-origin',
     'sec-fetch-mode': 'cors',
     'sec-fetch-dest': 'empty',
@@ -23,8 +27,26 @@ params = (
 )
 
 
+def chromedriver_exists():
+    chromedriver = subprocess.Popen("ls", shell=True, stdout=subprocess.PIPE)
+    chromedriver_return = chromedriver.stdout.read().decode('utf-8')
+    return True if 'chromedriver' in chromedriver_return else False
+
+
+def chromedriver_updated():
+    google_process = subprocess.Popen("google-chrome --version",
+                                      shell=True, stdout=subprocess.PIPE)
+    google_process_return = google_process.stdout.read().decode('utf-8')
+    chromedriver = subprocess.Popen("./chromedriver --version",
+                                    shell=True, stdout=subprocess.PIPE)
+    chromedriver_return = chromedriver.stdout.read().decode('utf-8')
+    return (True if google_process_return[14:18] ==
+            chromedriver_return[13:17] else False)
+
+
 def get_chrome_version():
-    google_process = subprocess.Popen("google-chrome --version", shell=True, stdout=subprocess.PIPE)
+    google_process = subprocess.Popen("google-chrome --version",
+                                      shell=True, stdout=subprocess.PIPE)
     google_process_return = google_process.stdout.read()
     return google_process_return.decode('utf-8')
 
@@ -45,14 +67,18 @@ def get_chromedriver_version(chrome_version: str):
 
 def download_chromedriver(chromedriver_version: str):
     headers = {
-        'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
+        'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91",'
+        '"Chromium";v="91"',
         'sec-ch-ua-mobile': '?0',
         'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
-        'Referer': f'https://chromedriver.storage.googleapis.com/index.html?path={chromedriver_version}/',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36'
+        '(KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
+        'Referer': 'https://chromedriver.storage.googleapis.com/'
+        f'index.html?path={chromedriver_version}/',
     }
 
-    url = f'https://chromedriver.storage.googleapis.com/{chromedriver_version}/chromedriver_linux64.zip'
+    url = 'https://chromedriver.storage.googleapis.com/'
+    f'{chromedriver_version}/chromedriver_linux64.zip'
 
     with requests.get(url, headers, stream=True) as r:
         r.raise_for_status()
@@ -61,16 +87,30 @@ def download_chromedriver(chromedriver_version: str):
                 if chunk:
                     f.write(chunk)
 
-    g = subprocess.Popen("rm chromedriver; unzip chromedriver.zip", shell=True, stdout=subprocess.PIPE)
+    g = subprocess.Popen("unzip chromedriver.zip && rm chromedriver.zip",
+                         shell=True, stdout=subprocess.PIPE)
     return g.stdout.read()
 
 
 def main():
-    g = get_chrome_version()
-    b = parse_version(g)
-    version = get_chromedriver_version(b)
-    download_chromedriver(version.group())
-    print(f'👌 Chromedriver updated: {version.group()}')
+    download = False
+    if(chromedriver_exists()):
+        print('Chromedriver already exists')
+        if(chromedriver_updated()):
+            print('Chromedriver already updated')
+        else:
+            download = True
+    else:
+        download = True
+
+    if (download is True):
+        g = get_chrome_version()
+        b = parse_version(g)
+        version = get_chromedriver_version(b)
+        download_chromedriver(version.group())
+        print(f'👌 Chromedriver updated: {version.group()}')
+    else:
+        pass
 
 
 if __name__ == '__main__':
